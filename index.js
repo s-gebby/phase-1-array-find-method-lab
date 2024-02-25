71e8731285990e795c5a3220768805f53676fc98
silas <silasgebhart12@gmail.com>
2024-02-25 14:29:51 -0600

    final commit

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+const record = [
+  { year: "2015", result: "W" },
+  { year: "2013", result: "L" },
+  { year: "2014", result: "N/A" },
+];
+
+function superbowlWin(records) {
+  for (let i = 0; i < records.length; i++) {
+    if (records[i].result === "W") {
+      return records[i].year;
+    }
+  }
+}
